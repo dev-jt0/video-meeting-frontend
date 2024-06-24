8b9dcf06bdc88149e102b32504cd049d0e116fba
dev-jt0 <dreamstar0209@gmail.com>
2024-06-25 00:26:32 +0800

    update: config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,5 @@
 // const SERVER_URL = 'http://localhost:3001';
 
-const SERVER_URL = 'https://video-meeting.deamtest.com';
+const SERVER_URL = "https://video-meeting-backend.onrender.com";
 
-export {
-    SERVER_URL
-}
+export { SERVER_URL };
